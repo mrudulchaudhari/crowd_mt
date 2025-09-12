f250d131f5b5a9ff7189ac7d79c2ba5225f22f7b
Mrudul <mrudulchaudhari0608@gmail.com>
2025-09-13 01:23:09 +0530

    for main ref

--- backend/crowd_mgmt/asgi.py
+++ backend/crowd_mgmt/asgi.py
@@ -1,29 +1,17 @@
-"""
-ASGI config for crowd_mgmt project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
+# backend/core/crowd_mgmt/asgi.py
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import core.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+# import app routing
+import core.routing as core_routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crowd_mgmt.settings")
 
-# This is the key change.
-# It tells Django to use the standard application for HTTP requests,
-# but to use your custom URLRouter for WebSocket connections.
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            core.routing.websocket_urlpatterns
-        )
+        URLRouter(core_routing.websocket_urlpatterns)
     ),
 })
-
